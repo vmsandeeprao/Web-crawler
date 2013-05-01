3ddea19c7275d071889e44e3cd5a15f06c37e8fe
sandeep rao <vmsandeeprao@gmail.com>
2013-05-02 00:57:57 +0530

    added crawl.py first time

---
+++ crawl.py
@@ -0,0 +1,26 @@
+from urllib2 import *
+import re
+
+regex = re.compile('<a\s*href=[\'|"](.*?)[\'"].*?>')
+tocrawl = set(['http://www.topcoder.com/tc'])
+crawled = set([])
+
+while 1:
+    crawling = tocrawl.pop()
+    if '.mp3' in crawling or '.png' in crawling:
+        crawled.add(crawling)
+        continue
+    if crawling in crawled:
+        continue
+    print 'crawling...' + crawling
+    try:
+        contents = urlopen(crawling)
+    except:
+        continue
+    c = contents.read()
+    crawled.add(crawling)
+
+    links = regex.findall(c)
+    for link in links:
+        if link.startswith('http') and link not in crawled:
+            tocrawl.add(link)
